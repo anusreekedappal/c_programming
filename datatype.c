efc2d56d6c0b4c748405a85f4085527d64e93a90
anusreekedappal <57188609+anusreekedappal@users.noreply.github.com>
2021-11-14 20:05:01 +0530

    Add files via upload

---
+++ datatype.c
@@ -0,0 +1,12 @@
+//Write a program to print the size of int, long int, double, char, float.
+
+#include<stdio.h>
+int main()
+{
+printf("the size of int=%lu\n",sizeof(int));
+printf("the size of iongint=%lu\n",sizeof(long int));
+printf("the size of double=%lu\n ",sizeof(double));
+printf("the size of char=%lu\n",sizeof(char));
+printf("the size of float= %lu\n",sizeof(float));
+return 0;
+}
